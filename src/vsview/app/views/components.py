Uranite/vs-view

commit 7aecfc022471a9e18ed78956b5993b6e8c879d5a

workspace: add timeout to disable loaded page on slow frame renders

src/vsview/app/workspace/loader.py
@@ -13,7 +13,7 @@ from types import ModuleType
 from typing import Any, ClassVar, Literal, assert_never
 
 from jetpytools import clamp, fallback
-from PySide6.QtCore import QSignalBlocker, Qt, Signal
+from PySide6.QtCore import QSignalBlocker, Qt, QTimer, Signal
 from PySide6.QtWidgets import (
     QApplication,
     QHBoxLayout,
@@ -597,20 +597,26 @@ class LoaderWorkspace[T](BaseWorkspace):
         previous_voutput = self.outputs_manager.voutputs[self.tab_manager.tabs.previous_tab_index]
         current_voutput = self.outputs_manager.current_voutput
 
+        timer_disable = None
         if (
             not (previous_voutput.last_frame == current_voutput.last_frame == target_frame)
             or not current_voutput.loaded_once
         ):
+            current_voutput.loaded_once = True
             if not seamless:
                 if (prev_pixmap := self.tab_manager.previous_view.pixmap_item.pixmap()).isNull():
                     self.set_loading_page()
                 else:
-                    self.loaded_page.setDisabled(True)
+                    # Disable the loaded page if the frame takes too much time (50 ms) to render
+                    timer_disable = QTimer(self, singleShot=True, timerType=Qt.TimerType.PreciseTimer)
+                    timer_disable.timeout.connect(lambda: self.loaded_page.setDisabled(True))
+                    timer_disable.start(50)
                     self.tab_manager.update_current_view(prev_pixmap)
-            current_voutput.loaded_once = True
 
             def on_complete(f: Future[None]) -> None:
                 if not f.exception():
+                    if timer_disable:
+                        timer_disable.stop()
                     self.loaded_page.setEnabled(True)
                     self.set_loaded_page()
 

commit 9bba9475f77417ee49340e0d4893e5b31d5e4ddb

workspace: use BlockableWidget to prevent user interaction
Fixes #106, bug introduced in 7aecfc022471a9e18ed78956b5993b6e8c879d5a

src/vsview/app/views/components.py
@@ -1,9 +1,12 @@
 from collections.abc import Iterator, Sequence
 from contextlib import contextmanager
+from typing import Any
 
+from jetpytools import copy_signature
 from PySide6.QtCore import (
     QAbstractTableModel,
     QEasingCurve,
+    QEvent,
     QModelIndex,
     QPoint,
     QPointF,
@@ -16,7 +19,7 @@ from PySide6.QtCore import (
     Signal,
     Slot,
 )
-from PySide6.QtGui import QBrush, QColor, QMouseEvent, QPainter, QPaintEvent, QPalette, QShowEvent
+from PySide6.QtGui import QBrush, QColor, QMouseEvent, QPainter, QPaintEvent, QPalette, QResizeEvent, QShowEvent
 from PySide6.QtWidgets import (
     QBoxLayout,
     QButtonGroup,
@@ -35,6 +38,113 @@ from PySide6.QtWidgets import (
 )
 
 from ...assets import loading_icon
+
+
+class _OverlayWidget(QWidget):
+    def __init__(self, parent: QWidget | None = None) -> None:
+        super().__init__(parent)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setVisible(False)
+
+    def event(self, event: QEvent) -> bool:
+        if event.type() in (
+            QEvent.Type.MouseButtonPress,
+            QEvent.Type.MouseButtonRelease,
+            QEvent.Type.MouseButtonDblClick,
+            QEvent.Type.MouseMove,
+            QEvent.Type.Wheel,
+            QEvent.Type.HoverEnter,
+            QEvent.Type.HoverLeave,
+            QEvent.Type.HoverMove,
+            QEvent.Type.TabletPress,
+            QEvent.Type.TabletRelease,
+            QEvent.Type.TabletMove,
+            QEvent.Type.TouchBegin,
+            QEvent.Type.TouchUpdate,
+            QEvent.Type.TouchEnd,
+            QEvent.Type.TouchCancel,
+            QEvent.Type.ContextMenu,
+        ):
+            event.accept()
+            return True
+        return super().event(event)
+
+
+class BlockableWidget(QWidget):
+    """
+    A widget that can block all user interactions (mouse, keyboard, etc.)
+    """
+
+    @copy_signature(QWidget.__init__)
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self._blocked = False
+        self._overlay = _OverlayWidget(self)
+
+    def setEnabled(self, arg__1: bool) -> None:
+        self.blocked = not arg__1
+        return super().setEnabled(arg__1)
+
+    def setDisabled(self, arg__1: bool) -> None:
+        self.blocked = arg__1
+        return super().setDisabled(arg__1)
+
+    @contextmanager
+    def block(self) -> Generator[None]:
+        was_blocked = self._blocked
+        self.set_blocked(True)
+        try:
+            yield
+        finally:
+            self.set_blocked(was_blocked)
+
+    def set_blocked(self, value: bool) -> None:
+        if self._blocked != value:
+            self._blocked = value
+            if value:
+                # Clear focus from any active descendant to prevent keyboard input
+                window = self.window()
+                if window:
+                    focused = window.focusWidget()
+                    if focused and (focused is self or self.isAncestorOf(focused)):
+                        focused.clearFocus()
+                self._overlay.setGeometry(self.rect())
+                self._overlay.show()
+                self._overlay.raise_()
+            else:
+                self._overlay.hide()
+            self.update()
+
+    @property
+    def blocked(self) -> bool:
+        return self._blocked
+
+    @blocked.setter
+    def blocked(self, value: bool) -> None:
+        self.set_blocked(value)
+
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        super().resizeEvent(event)
+        self._overlay.setGeometry(self.rect())
+
+    def focusNextPrevChild(self, next: bool) -> bool:
+        # Prevent tab focus transition when blocked
+        if self._blocked:
+            return False
+        return super().focusNextPrevChild(next)
+
+    def event(self, event: QEvent) -> bool:
+        if self._blocked and event.type() in (
+            QEvent.Type.KeyPress,
+            QEvent.Type.KeyRelease,
+            QEvent.Type.FocusIn,
+            QEvent.Type.FocusOut,
+            QEvent.Type.Shortcut,
+            QEvent.Type.ShortcutOverride,
+        ):
+            event.accept()
+            return True
+        return super().event(event)
 
 
 class SegmentedControl(QWidget):

src/vsview/app/workspace/loader.py
@@ -34,7 +34,7 @@ from ..outputs import AudioOutput, OutputsManager, VideoOutput
 from ..plugins.api import PluginAPI, WidgetPluginBase
 from ..settings import ActionID, ShortcutManager
 from ..views import PluginDock, PluginSplitter
-from ..views.components import CustomLoadingPage, DockButton
+from ..views.components import BlockableWidget, CustomLoadingPage, DockButton
 from ..views.tab import TabViewWidget
 from ..views.timeline import TimelineControlBar
 from ..views.video import ViewState
@@ -101,7 +101,7 @@ class LoaderWorkspace[T](BaseWorkspace):
         self.stack.addWidget(self.loading_page)
 
         # Loaded State
-        self.loaded_page = QWidget(self)
+        self.loaded_page = BlockableWidget(self)
         self.loaded_layout = QVBoxLayout(self.loaded_page)
         self.loaded_layout.setContentsMargins(0, 0, 0, 0)
         self.loaded_layout.setSpacing(0)
@@ -618,6 +618,7 @@ class LoaderWorkspace[T](BaseWorkspace):
                     self.set_loading_page()
                 else:
                     # Disable the loaded page if the frame takes too much time (50 ms) to render
+                    self.loaded_page.blocked = True
                     timer_disable = QTimer(self, singleShot=True, timerType=Qt.TimerType.PreciseTimer)
                     timer_disable.timeout.connect(lambda: self.loaded_page.setDisabled(True))
                     timer_disable.start(50)